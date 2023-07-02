d9b9d3f1083cfbfab7eab4b12db8396a7de98f0f
Jean Schram <jgssfw@gmail.com>
2023-07-02 13:08:04 -0300

    Commitando o projeto completo

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+const listaSelecaoPokemons = document.querySelectorAll('.pokemon')
+const pokemonsCard = document.querySelectorAll('.cartao-pokemon')
+
+listaSelecaoPokemons.forEach(pokemon => {
+    pokemon.addEventListener('click', () => { //faz evento para click
+        const cartaoPokemonAberto = document.querySelector('.aberto') //encontrar o pokemon aberto
+        cartaoPokemonAberto.classList.remove('aberto') // remover o atributo "aberto"
+
+        const idPokemonSelecionado = pokemon.attributes.id.value
+
+        const idDoCartaoPokemonParaAbrir = 'cartao-' + idPokemonSelecionado
+        const cartaoPokemonParaAbrir = document.getElementById(idDoCartaoPokemonParaAbrir)
+        cartaoPokemonParaAbrir.classList.add('aberto')
+
+        const pokemonAtivoNaListagem = document.querySelector('.ativo')
+        pokemonAtivoNaListagem.classList.remove('ativo')
+
+        const pokemonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado)
+        pokemonSelecionadoNaListagem.classList.add('ativo')
+    })
+})
